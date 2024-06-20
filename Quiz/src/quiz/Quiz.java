3ead23cdf763bcf1a0d4b831d0137c23efa76c60
DavidG <davidgonzalezlda497@gmail.com>
2024-06-19 19:59:08 -0600

    Quiz Final

--- Quiz/src/quiz/Quiz.java
+++ Quiz/src/quiz/Quiz.java
@@ -3,18 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package quiz;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author Aulas Heredia
  */
 public class Quiz {
-
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       /*Se crea el mensaje donde se solicita el salario, cantidad de empleados*/
+       int cantidadEmpleados; 
+        cantidadEmpleados = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la cantidad de empleados que hay en la empresa: ")); 
+        
+        
+        double totalPagar= 0;
+        
+        for (int i = 0; i < cantidadEmpleados; i++) {
+           double salario= Double.parseDouble(JOptionPane.showInputDialog(null,"Digite su salario "+(i+ 1)+":"));
+           
+        double sem= salario* 0.0925;
+       double ivm= salario* 0.0508; 
+       
+       
+       totalPagar += sem + ivm; 
+       
+      
+            
+        }
+       JOptionPane.showMessageDialog(null,"El total a pagar es de:"+ totalPagar+"₡");
+        
+       
+       
+       
+       
+    }
+       
+       
     }
     
-}
+   
+
